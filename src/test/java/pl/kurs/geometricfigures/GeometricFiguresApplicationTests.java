08125aae44868f83ee4148abbd17a77082952e3f
Piotr <delesek0@op.pl>
2022-12-12 00:48:35 +0100

    first-commit

---
+++ src/test/java/pl/kurs/geometricfigures/GeometricFiguresApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.geometricfigures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeometricFiguresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
